39b246d9f1c92867a54ee2ff446c1f454799f3db
tangbao007 <654332616@qq.com>
2019-05-09 14:07:06 +0800

    111

--- spring-boot-mybatis-xml-mulidatasource/src/main/java/com/neo/MXMApplication.java
+++ spring-boot-mybatis-xml-mulidatasource/src/main/java/com/neo/MXMApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MXMApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MXMApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MXMApplication.class, args);
+        System.out.println("123");
+    }
 }
